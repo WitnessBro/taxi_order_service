0f258cfce43224284dd26b809601d6bcc20e5354
wtfbro <wtfbro@yandex-team.ru>
2024-06-21 13:20:03 +0300

    add cobra to project

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
-package taxi_order_service
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorldHandler(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorldHandler)
+	http.ListenAndServe(":8080", nil)
+}
