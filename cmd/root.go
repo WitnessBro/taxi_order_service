40dcdc9e4fd3c568c9638305787fb824fc3e8d26
wtfbro <wtfbro@yandex-team.ru>
2024-06-21 15:53:18 +0300

    review fix [3] move all server stuff to runserver.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,13 @@
 package cmd
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/spf13/cobra"
+	"taxi_order_service/cmd/runserver"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "taxi_order_service",
 	Short: "A brief description of your application",
@@ -31,26 +30,6 @@ func Execute() {
 	}
 }
 
-var runserverCmd = &cobra.Command{
-	Use:   "runserver",
-	Short: "Запускает какой бы то ни было сервер:)",
-	Long: `Более длинное описание того, что делает команда.
-		Несколько строк текста.
-		Ага, третья строка`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("runserver called")
-
-		flag, err := cmd.Flags().GetString("test")
-
-		if flag != "" {
-			fmt.Println("runserver called with flag: ", flag)
-		}
-		return err
-	},
-}
-
 func init() {
-	RootCmd.AddCommand(runserverCmd)
-	runserverCmd.Flags().String("test", "", "just test flag")
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.AddCommand(runserver.Cmd)
 }
--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"taxi_order_service/cmd"
-	"taxi_order_service/cmd/runserver"
 )
 
 func main() {
 	cmd.Execute()
-	runserver.RunServer()
 }
